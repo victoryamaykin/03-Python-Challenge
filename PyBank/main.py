870b3d0011d7c7c16cb9074917f8b65e220f849d
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-02-22 15:28:47 -0500

    Add files via upload
    Updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,11 @@ import csv
 
 # Variables
 months = []
-#profit-loses 
+#profit-losses 
 p = []
-average_change = 0
+average_net_change = 0
 total_months = 0 
+net_change = []
 
 # Load csv file 
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
@@ -24,33 +25,27 @@ with open(csvpath, newline="") as csvfile:
             values = int(row[1])
             p.append(values)
 
-# Find the total of months in the entire period 
 total_months = len(months)
+net_total = sum(p)
+net_total_months =len(months)-1 
+difference_budget_data = []
+for i in range(len(p)-1):
+        difference_budget_data.append(float(p[i + 1])-float(p[i]))
+        new_net_total = sum(difference_budget_data)
+# Find the sum of profits/losses
 
-# Find the average of changes in Profits/Losses 
-net_total = []
-
-for n in p:
-        change = p[n] - p[n - 1]
-        net_total.append(change) 
-
-def average(net_total):
-    length = len(net_total)
-    net_total = sum(net_total)
-    return net_total /length 
-
-average_change = int(average(net_total))
+average_net_change = new_net_total/net_total_months
 
 # Find the greatest increase/decrease (date and amount) over the entire period
-min_p = p[p.index(min(p))]
-max_p = p[p.index(max(p))]
+min_p = p[p.index(min(p))] - p[p.index(min(p))-1]
+max_p = p[p.index(max(p))] - p[p.index(max(p))-1]
 
 # Print out results to console
 print("Financial Analysis")
 print("--------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net_total}")
-print(f"Average Change: ${average_change}")
+print(f"Average Change: ${round(average_net_change,2)}")
 print(f'Greatest Increase in Profits: {months[p.index(max(p))]} (${max_p})')
 print(f"Greatest Descrease in Profits: {months[p.index(min(p))]} (${min_p})")
 
@@ -62,6 +57,6 @@ with open(output_file, "w", newline="") as datafile:
     csvwriter.writerow(["--------------------"])
     csvwriter.writerow([f"Total Months: {total_months}"])
     csvwriter.writerow([f"Total: ${net_total}"])
-    csvwriter.writerow([f"Average Change: ${average_change}"])
+    csvwriter.writerow([f"Average Change: ${average_net_change}"])
     csvwriter.writerow([f'Greatest Increase in Profits: {months[p.index(max(p))]} (${max_p})'])
     csvwriter.writerow([f"Greatest Descrease in Profits: {months[p.index(min(p))]} (${min_p})"])
